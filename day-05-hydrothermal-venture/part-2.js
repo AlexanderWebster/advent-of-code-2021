7b53de972d3b61dd2390fb74c5af24cefe779650
Alexander <awebst10@kent.edu>
2021-12-05 21:51:07 -0600

    Formats switch statement.

--- day-05-hydrothermal-venture/part-2.js
+++ day-05-hydrothermal-venture/part-2.js
@@ -63,26 +63,26 @@ const main = () => {
 
     // enumerate points between line coords
     switch (line.orientation) {
-    case HORIZONTAL: {
+      case HORIZONTAL: {
         const range = rangeInclusive({ a: line.x1, b: line.x2 });
         points = range.map((point) => [point, line.y1]);
         break;
-        }
-        case VERTICAL: {
+      }
+      case VERTICAL: {
         const range = rangeInclusive({ a: line.y1, b: line.y2 });
         points = range.map((point) => [line.x1, point]);
         break;
-        }
+      }
       case DIAGONAL: {
         const lineLength = Math.abs(line.x1 - line.x2);
-        const xMovement = (line.x1 - line.x2) * -1 / lineLength;
-        const yMovement = (line.y1 - line.y2) * -1 / lineLength;
+        const xMovement = ((line.x1 - line.x2) * -1) / lineLength;
+        const yMovement = ((line.y1 - line.y2) * -1) / lineLength;
 
-        let point = { x: line.x1, y: line.y1};
+        let point = { x: line.x1, y: line.y1 };
         for (let i = 0; i <= lineLength; ++i) {
-            points.push([point.x, point.y]);
-            point.x += xMovement;
-            point.y += yMovement;
+          points.push([point.x, point.y]);
+          point.x += xMovement;
+          point.y += yMovement;
         }
         break;
       }
